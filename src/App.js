25b88a3bd0b1a1a7deb6598b5e27d0d73f45f9cd
Tim Yang <tyang098@gmail.com>
2022-10-08 15:41:07 -0400

    adding banner:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React  from 'react';
-
 import {NavBar} from '../src/components/NavBar'
+import {Banner} from './components/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
